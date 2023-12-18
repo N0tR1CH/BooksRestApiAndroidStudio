9841d165a48b4067ca76ebad8bc9633e19785752
N0tR1CH <damianrichter58@gmail.com>
2023-12-18 21:05:40 +0100

    Add book holder and book adapter to main activity

--- app/src/main/java/com/example/restapiandroidproject/MainActivity.java
+++ app/src/main/java/com/example/restapiandroidproject/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.restapiandroidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,83 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean checkNullOrEmpty(String str) {
+        return str != null && !str.isEmpty();
+    }
+
+    private class BookHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
+
+        private final TextView bookTitleTextView;
+        private final TextView bookAuthorTextView;
+        private final TextView numberOfPagesTextView;
+        private final ImageView bookCover;
+
+        public BookHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.book_list_item, parent, false));
+
+            bookTitleTextView = itemView.findViewById(R.id.book_title);
+            bookAuthorTextView = itemView.findViewById(R.id.book_author);
+            numberOfPagesTextView = itemView.findViewById(R.id.number_of_pages);
+            bookCover = itemView.findViewById(R.id.img_cover);
+        }
+
+        public void bind(Book book) {
+            if (book != null && checkNullOrEmpty(book.getTitle()) && book.getAuthors() != null) {
+                bookTitleTextView.setText(book.getTitle());
+                bookAuthorTextView.setText(TextUtils.join(", ", book.getAuthors()));
+                numberOfPagesTextView.setText(book.getNumberOfPages());
+
+                if (book.getCover() != null) {
+                    Picasso.with(itemView.getContext())
+                            .load(IMAGE_URL_BASE + book.getCover() + "-S.jpg")
+                            .placeholder(R.drawable.baseline_book_24)
+                            .into(bookCover);
+                } else {
+                    bookCover.setImageResource(R.drawable.baseline_book_24);
+                }
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            // TODO: Implement onClick
+        }
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder> {
+        private List<Book> books;
+
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new BookHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position) {
+            if (books != null) {
+                Book book = books.get(position);
+                holder.bind(book);
+            } else {
+                Log.d("MainActivity", "No books");
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            if (books != null) {
+                return books.size();
+            } else {
+                return 0;
+            }
+        }
+
+        void setBooks(List<Book> books) {
+            this.books = books;
+            notifyDataSetChanged();
+        }
     }
 }
