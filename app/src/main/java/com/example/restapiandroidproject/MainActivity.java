764aeb0891278db164c72991750ffce8276ae0f9
N0tR1CH <damianrichter58@gmail.com>
2023-12-18 21:28:28 +0100

    Finish steps from document

--- app/src/main/java/com/example/restapiandroidproject/MainActivity.java
+++ app/src/main/java/com/example/restapiandroidproject/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.restapiandroidproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +34,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.book_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchBooksData(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void fetchBooksData(String query) {
+        String finalQuery = prepareQuery(query);
+        BookService bookService = RetroFitInstance.getRetrofitInstance().create(BookService.class);
+
+        Call<BookContainer> booksApiCall = bookService.findBooks(finalQuery);
+
+        booksApiCall.enqueue(new Callback<BookContainer>() {
+            @Override
+            public void onResponse(@NonNull Call<BookContainer> call, @NonNull Response<BookContainer> response) {
+                if (response.body() != null) {
+                    setupBookListView(response.body().getBookList());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<BookContainer> call, @NonNull Throwable t) {
+                Snackbar.make(
+                        findViewById(R.id.main_view),
+                        "Something went wrong... Please try later!",
+                        BaseTransientBottomBar.LENGTH_LONG
+                ).show();
+            }
+        });
+    }
+
+    private String prepareQuery(String query) {
+        String[] queryParts = query.split("\\s+");
+        return TextUtils.join("+", queryParts);
+    }
+
+    private void setupBookListView(List<Book> books) {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        final BookAdapter bookAdapter = new BookAdapter();
+
+        recyclerView.setAdapter(bookAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public boolean checkNullOrEmpty(String str) {
