6e79993ed6c34a45430b543d60fb654d94879f81
N0tR1CH <damianrichter58@gmail.com>
2023-12-18 19:07:35 +0100

    Modify author_name so that it is list of strings

--- app/src/main/java/com/example/restapiandroidproject/Book.java
+++ app/src/main/java/com/example/restapiandroidproject/Book.java
@@ -2,11 +2,13 @@ package com.example.restapiandroidproject;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class Book {
     @SerializedName("title")
     private String title;
     @SerializedName("author_name")
-    private String authorName;
+    private List<String> authors;
     @SerializedName("cover_i")
     private String cover;
     @SerializedName("number_of_pages_median")
